f600f92bc72df148af33ba8fd07251ff514d57af
Alex <conter10@mail.ru>
2021-03-16 08:40:20 +0100

    refactoring

--- src/lib/notification/play-notification.ts
+++ src/lib/notification/play-notification.ts
@@ -3,7 +3,7 @@ import {Audio} from 'expo-av'
 export const playSoundNotification = async () => {
     try {
         const {sound: soundObject, status} =
-            await Audio.Sound.createAsync(require('../../../notif.mp3'), {shouldPlay: true})
+            await Audio.Sound.createAsync(require('../../../assets/audio/notif.mp3'), {shouldPlay: true})
     } catch (error) {
         console.log('Error: ' + error)
     }
